1557f59048ec4726e6e31911e67d023d91869cd1
Leo Lebedev <ptichkalebed15@gmail.com>
2022-06-28 19:45:49 +0300

    add todo list app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container flex items-center justify-center bg-teal-lightest font-sans w-full">
+    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+      <div class="mb-4">
+        <h1 class="text-gray-900 font-bold text-2xl">Todo List</h1>
+        <div class="flex mt-4">
+          <input
+              v-model.trim="newTodo"
+              class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-800"
+              placeholder="Add Todo"
+              @keyup.enter="AddNewTodo">
+          <button
+              class="flex-no-shrink p-2 border-2 rounded text-teal-500 border-teal-500 hover:text-white hover:bg-teal-500"
+              @click="AddNewTodo">
+            Add
+          </button>
+        </div>
+      </div>
+      <div>
+        <div v-if="todos.length === 0">
+          <p class="text-gray-900 font-bold text-l">To-do list is empty. Add a new to-do</p>
+        </div>
+
+        <todo-list
+            v-for="todo in todos"
+            :key="todo"
+            :todos="todo"
+            @remove-todo="deleteTodo"
+        />
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "@/components/TodoList";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {TodoList},
+  data() {
+    return {
+      todos: ['Buy products', 'Go to the cinema', 'Do homework'],
+      newTodo: '',
+    }
+  },
+  methods: {
+    AddNewTodo() {
+      if (this.newTodo.length > 0) {
+        this.todos.push(this.newTodo)
+        this.newTodo = ''
+      }
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  },
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex mb-4 items-center">
+    <p :class="doneClass">
+      {{ todos }}
+    </p>
+    <button
+        class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-500 border-green-500 hover:bg-green-500"
+        @click="todoDone">
+      Done
+    </button>
+    <button
+        class="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500"
+        @click="$emit('removeTodo')">
+      Remove
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: ['todos'],
+  emit: [],
+  data() {
+    return {
+      done: false,
+    }
+  },
+  methods: {
+    todoDone() {
+      this.done = !this.done;
+    },
+  },
+  computed: {
+    doneClass() {
+      if (this.done) {
+        return 'w-full line-through text-green-500'
+      }
+      return 'w-full text-grey-darkest'
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
